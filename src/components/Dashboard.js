f718fbe6014bf8b7ed140adf7395528a89d3b214
johnloberger <johnloberger@gmail.com>
2020-08-21 11:31:25 -0500

    prettify and remove console logs

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,61 +1,60 @@
-import React from 'react';
-import { connect } from 'react-redux'
-import { fetchLocationsSuccess } from '../actions/location'
-import { fetchStatSuccess } from '../actions/stat'
-import { fetchCitiesSuccess } from '../actions/city'
-import { currentUser } from '../actions/auth'
-import ClimateMap from './ClimateMap'
-import climScores from './climScores.json'
+import React from "react";
+import { connect } from "react-redux";
+import { fetchLocationsSuccess } from "../actions/location";
+import { fetchStatSuccess } from "../actions/stat";
+import { fetchCitiesSuccess } from "../actions/city";
+import { currentUser } from "../actions/auth";
+import ClimateMap from "./ClimateMap";
+import climScores from "./climScores.json";
 
 class Dashboard extends React.Component {
-
   state = {
     climateScores: climScores,
-  }
-  
-  componentDidMount(){
-    const token = localStorage.getItem('token')
-    console.log(token)
+  };
+
+  componentDidMount() {
+    const token = localStorage.getItem("token");
     if (!token) {
-      this.props.history.push('/login')
+      this.props.history.push("/login");
     } else {
       const reqObj = {
-        method: 'GET',
+        method: "GET",
         headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      }
-      fetch('http://localhost:3001/current_user', reqObj)
-      .then(resp => resp.json())
-      .then(data => {
-        console.log(data)
-        this.props.currentUser(data)
-      })
-      fetch('https://api.waqi.info/map/bounds/?latlng=85,-180,-85.05115,180&token=87b2bba6a5b2e26c577ffc48e297eaed82a8408c')
-      .then(resp => resp.json())
-      .then(stations => {
-        let y = stations.data
-        let x = stations.data.slice(0,4500)
-        console.log(y)
-        this.props.fetchStatSuccess(x)
-      })
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      fetch("http://localhost:3001/current_user", reqObj)
+        .then((resp) => resp.json())
+        .then((data) => {
+          this.props.currentUser(data);
+        });
+      fetch(
+        "https://api.waqi.info/map/bounds/?latlng=85,-180,-85.05115,180&token=87b2bba6a5b2e26c577ffc48e297eaed82a8408c"
+      )
+        .then((resp) => resp.json())
+        .then((stations) => {
+          let y = stations.data;
+          let x = stations.data.slice(0, 4500);
+          this.props.fetchStatSuccess(x);
+        });
     }
   }
 
-  render(){ 
-    console.log(this.props)
-    if (this.props.stats.length !== 0){
-    return (
-      <div>
-       
-        <ClimateMap cities={this.props.cities} climateScores={this.state.climateScores}/>
-      </div>)
+  render() {
+    if (this.props.stats.length !== 0) {
+      return (
+        <div>
+          <ClimateMap
+            cities={this.props.cities}
+            climateScores={this.state.climateScores}
+          />
+        </div>
+      );
     } else {
-      return null
+      return null;
     }
-  };
+  }
 }
-
 
 const mapStateToProps = (state) => {
   return {
@@ -64,16 +63,14 @@ const mapStateToProps = (state) => {
     locations: state.locations,
     stats: state.stats,
     cities: state.cities,
-  }
-}
+  };
+};
 
 const mapDispatchToProps = {
   fetchLocationsSuccess,
   fetchStatSuccess,
   fetchCitiesSuccess,
-  currentUser
-}
+  currentUser,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
-
-

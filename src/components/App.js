e7a57bde5005f3a71e8336b7649b9256efef131e
johnloberger <johnloberger@gmail.com>
2020-09-11 22:19:27 -0500

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../App.css';
 import NavBar from './NavBar';
-import { Route, Redirect, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 import LandingPage from './LandingPage'
 
 
--- src/components/DonateDashboard.js
+++ src/components/DonateDashboard.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
 import { currentUser } from "../actions/auth";
+const {TeamTrees} = require('teamtrees-api');
+const teamTrees = new TeamTrees();
 
 class Dashboard extends React.Component {
 
   render() {
+    teamTrees.getTotalTrees(true).then(console.log);
     return <div>
       <h2 style={{color: '#FFFFFF'}}>Ability to donate to #TeamTrees API is coming very soon!</h2>
     </div>
